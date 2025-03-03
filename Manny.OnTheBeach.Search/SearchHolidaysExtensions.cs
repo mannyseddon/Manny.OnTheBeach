ebcf7b52fffe2db0d9ea15b12f0abaab726b257c
Manny Seddon <mannyrecruit@hotmail.com>
2025-03-03 23:07:28 +0000

    added code to load hotels and flights

--- Manny.OnTheBeach.Search/SearchHolidaysExtensions.cs
+++ Manny.OnTheBeach.Search/SearchHolidaysExtensions.cs
@@ -1,12 +1,32 @@
-﻿using Manny.OnTheBeach.Models;
+﻿using Manny.OnTheBeach.FileReader;
+using Manny.OnTheBeach.Models;
+using Manny.OnTheBeach.Models.Data;
 
 namespace Manny.OnTheBeach.Search
 {
     public static class SearchHolidaysExtensions
     {
+        private const string FlightsFilePath = "flights.json";
+        private const string HotelsFilePath = "hotels.json";
+        private const string LocalFilePath = @"..\..\..\..\";
+
         public static async Task<List<HolidaySearchResults>> SearchHolidaysAsync(this HolidayRequirements holidayRequirements)
         {
-            throw new NotImplementedException();
+            var flightsData = Path.Combine(LocalFilePath, FlightsFilePath);
+            var hotelsData = Path.Combine(LocalFilePath, HotelsFilePath);
+
+            var flights = await flightsData.GetDataFromFileAsync<Flight>();
+            var hotels = await hotelsData.GetDataFromFileAsync<Hotel>();
+
+            var results = new List<HolidaySearchResults>
+            {
+                new HolidaySearchResults()
+            };
+
+
+
+
+            return results;
         }
     }
 }
