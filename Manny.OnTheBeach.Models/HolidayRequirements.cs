913fe66b14a9a09318c3b5cce8f65c372b4d3565
Manny Seddon <mannyrecruit@hotmail.com>
2025-03-04 15:38:56 +0000

    added unit tests for customer 2 and 3

--- Manny.OnTheBeach.Models/HolidayRequirements.cs
+++ Manny.OnTheBeach.Models/HolidayRequirements.cs
@@ -2,7 +2,7 @@
 
 public class HolidayRequirements
 {
-    public string DepartingFrom { get; set; } = string.Empty;
+    public List<string> DepartingFrom { get; set; } = new List<string>();
     public string TravelingTo { get; set; } = string.Empty;
     public DateOnly DepartureDate { get; set; } = DateOnly.MinValue;
     public int Duration { get; set; }
--- Manny.OnTheBeach.Tests/HolidayResultsTests.cs
+++ Manny.OnTheBeach.Tests/HolidayResultsTests.cs
@@ -13,7 +13,7 @@ namespace Manny.OnTheBeach.Tests
             // Arrange
             var requirements = new HolidayRequirements
             {
-                DepartingFrom = "MAN",
+                DepartingFrom = new List<string> { "MAN" },
                 TravelingTo = "AGP",
                 DepartureDate = new DateOnly(2023, 7, 1),
                 Duration = 7
@@ -30,6 +30,59 @@ namespace Manny.OnTheBeach.Tests
             Assert.That(results.Count, Is.Not.EqualTo(0));
             Assert.That(bestResult.Flight.Id, Is.EqualTo(2));
             Assert.That(bestResult.Hotel.Id, Is.EqualTo(9));
+            Assert.That(bestResult.TotalPrice, Is.EqualTo(826));
+            Assert.That(results, Is.InstanceOf<List<HolidaySearchResults>>());
+        }
+
+        [Test]
+        public async Task SearchHolidaysAsync_Customer2_ReturnsExpectedResult()
+        {
+            // Arrange
+            var requirements = new HolidayRequirements
+            {
+                DepartingFrom = new List<string> { "LTN", "LGW" },
+                TravelingTo = "PMI",
+                DepartureDate = new DateOnly(2023, 06, 15),
+                Duration = 10
+            };
+
+            var results = new List<HolidaySearchResults>();
+
+            //Act
+            results = await requirements.SearchHolidaysAsync();
+            var bestResult = results.First();
+
+            Assert.That(results, Is.Not.Null);
+            Assert.That(results.Count, Is.Not.EqualTo(0));
+            Assert.That(bestResult.Flight.Id, Is.EqualTo(6));
+            Assert.That(bestResult.Hotel.Id, Is.EqualTo(5));
+            Assert.That(bestResult.TotalPrice, Is.EqualTo(675));
+            Assert.That(results, Is.InstanceOf<List<HolidaySearchResults>>());
+        }
+
+        [Test]
+        public async Task SearchHolidaysAsync_Customer3_ReturnsExpectedResult()
+        {
+            // Arrange
+            var requirements = new HolidayRequirements
+            {
+                DepartingFrom = new List<string>(),
+                TravelingTo = "LPA",
+                DepartureDate = new DateOnly(2022, 11, 10),
+                Duration = 14
+            };
+
+            var results = new List<HolidaySearchResults>();
+
+            //Act
+            results = await requirements.SearchHolidaysAsync();
+            var bestResult = results.First();
+            
+            Assert.That(results, Is.Not.Null);
+            Assert.That(results.Count, Is.Not.EqualTo(0));
+            Assert.That(bestResult.Flight.Id, Is.EqualTo(7));
+            Assert.That(bestResult.Hotel.Id, Is.EqualTo(6));
+            Assert.That(bestResult.TotalPrice, Is.EqualTo(1175));
             Assert.That(results, Is.InstanceOf<List<HolidaySearchResults>>());
         }
     }
